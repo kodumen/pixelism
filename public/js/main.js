23cbd319908a84f1030c906434119df5f459b39e
kodumen <r.b.daitan@gmail.com>
2016-04-15 22:40:20 +0800

    Show app and hide splash.

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+/**
+ * Pixelism 1.0
+ */
+
+var app;
+var splash;
+
+/**
+ * Initialize the application.
+ */
+function init() {
+    app = document.querySelector('#app');
+    splash = document.querySelector('#splash');
+}
+
+/**
+ * Hide the splash and show the app.
+ */
+function showApp() {
+    app.removeAttribute('hidden');
+    splash.setAttribute('hidden', '');
+}
+
+init();
+showApp();
+
+console.log('APP LOADED');

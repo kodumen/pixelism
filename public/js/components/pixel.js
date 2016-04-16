6d27bb487fe273ca51c3a530c6af5914688bb396
kodumen <r.b.daitan@gmail.com>
2016-04-16 18:18:31 +0800

    Implement paint (black only)
    * Also fixed slowdown caused by too many rectangles.

--- public/js/components/pixel.js
+++ public/js/components/pixel.js
@@ -1,11 +1,14 @@
 var _PIX_SIZE = 32;
 
 var pixelModel = {
-    template: '<rect v-bind:x="pX" v-bind:y="pY" v-bind:fill="fill"></rect>',
+    template: '<rect v-bind:x="pX" v-bind:y="pY" v-bind:fill="fill" v-on:click="setFill"></rect>',
     props: ['x', 'y', 'fill'],
     computed: {
         pX: _pixelGetX,
         pY: _pixelGetY
+    },
+    methods: {
+        setFill: _pixelSetFill
     }
 };
 
@@ -21,4 +24,11 @@ function _pixelGetX() {
  */
 function _pixelGetY() {
     return this.y * _PIX_SIZE;
+}
+
+/**
+ * Set the fill of this pixel.
+ */
+function _pixelSetFill() {
+    this.fill = 'black';
 }

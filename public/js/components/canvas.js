4fdbcab9cdd9b89298870403522438efc833133e
kodumen <r.b.daitan@gmail.com>
2016-04-16 17:23:54 +0800

    Implemented toggle grid

--- public/js/components/canvas.js
+++ public/js/components/canvas.js
@@ -2,5 +2,15 @@ var canvasModel = {
     el: '#canvas',
     data: {
         isShowGrid: true
+    },
+    methods: {
+        toggleGrid: _canvasToggleGrid
     }
 };
+
+/**
+ * Turn grid on or off.
+ */
+function _canvasToggleGrid() {
+    this.isShowGrid = !this.isShowGrid;
+}

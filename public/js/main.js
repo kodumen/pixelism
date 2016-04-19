4d25948ebd20eb9362f8ceca6eef2d73fe3871af
kodumen <r.b.daitan@gmail.com>
2016-04-19 09:45:47 +0800

    Changed canvas size again

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ var shell;
  * Initialize the application.
  */
 function init() {
-    app = new Vue(new AppModel(new BlankCanvas(27, 48, 12)));
+    app = new Vue(new AppModel(new BlankCanvas(45, 80, 9)));
     shell = new Vue(shellModel);
 }
 
